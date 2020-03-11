00d51e66713e67f2f034301766eea14d3361e74d
SamyOptimize <samy@optimizekw.com>
2020-03-11 10:00:18 +0300

    Add build files

---
+++ android/app/build/generated/source/buildConfig/debug/host/exp/exponent/BuildConfig.java
@@ -0,0 +1,17 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package host.exp.exponent;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.photoeditor.expo";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+  // Fields from default config.
+  public static final boolean IMGLY_ENABLED = true;
+      
+      @ly.img.android.pesdk.annotations.StateEvents private static class IMGLY_PROCESSOR_INIT extends host.exp.exponent.IMGLYEvents {IMGLY_PROCESSOR_INIT(){super(null);}};
+}
--- android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -3,6 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "";
+  public static final String DEVELOPMENT_URL = "exp62aab0d8f7164991885e918076cb06f2://192.168.8.198:80";
 
 }
